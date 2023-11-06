4f908f203314e6213ba6c0be68205a73cf42bd0a
AndrewDevos <andrew.cottadevos@gmail.com>
2023-11-06 19:49:06 -0300

    commit publico

--- main.go
+++ main.go
@@ -2,42 +2,37 @@ package main
 
 import "fmt"
 
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
 func main() {
 
 	fmt.Println()
-	banco()
+
+	contaDaSilvia :=  new(ContaCorrente) // ou ContaCorrente{} 
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
+	fmt.Println(contaDaSilvia)
+
+	// valorDoSaque := 200.
 	
 
-}	//equivalente a init, self/ 'c' (poderia ser qualquer nome mas em go é a primeira letra do ponteiro) é o endereço de *ContaCorrente
+
+	fmt.Println(contaDaSilvia.Sacar(200))
+	fmt.Println("O valor do SALDO é: ", contaDaSilvia.saldo)
+
+
+} //equivalente a init, self/ 'c' (poderia ser qualquer nome mas em go é a primeira letra do ponteiro) é o endereço de *ContaCorrente
 func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque <= c.saldo
-	if podeSacar{
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
 		c.saldo -= valorDoSaque
 		return "Saque realizado com sucesso! "
 	} else {
 		return "Saldo insuficiente! "
 	}
 }
-func banco(Sacar()) {
-	type ContaCorrente struct {
-		titular       string
-		numeroAgencia int
-		numeroConta   int
-		saldo         float64
-	}
-	contaDoGuilherme := ContaCorrente{titular:"Guilherme", numeroAgencia: 5473, numeroConta: 326545, saldo: 200}
-	fmt.Println(contaDoGuilherme)
-	
-	contaDaCris := new(ContaCorrente)
-	fmt.Println(contaDaCris)
-
-	contaDaSilvia := new(ContaCorrente)
-	contaDaSilvia.titular = "Silvia"
-	contaDaSilvia.saldo = 500
-	fmt.Println(contaDaSilvia)
-
-	valorDoSaque := 200.
-	fmt.Println("O valor do saque é: ",valorDoSaque)
-	fmt.Println(contaDaSilvia.Sacar(300))
-}
-

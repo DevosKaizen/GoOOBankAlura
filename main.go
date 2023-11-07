891018c20e926b2df9b55bf3b799809f04bf8a2a
AndrewDevos <andrew.cottadevos@gmail.com>
2023-11-07 01:23:59 -0300

    final da aula de hoje 07/11/23

--- main.go
+++ main.go
@@ -25,14 +25,27 @@ func main() {
 	fmt.Println(contaDaSilvia.Sacar(200))
 	fmt.Println("O valor do SALDO é: ", contaDaSilvia.saldo)
 
+	status, valor := contaDaSilvia.Depositar(20.)
+	fmt.Println("O valor do SALDO é: ", contaDaSilvia.saldo)
 
-} //equivalente a init, self/ 'c' (poderia ser qualquer nome mas em go é a primeira letra do ponteiro) é o endereço de *ContaCorrente
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	fmt.Println(status, valor)
+
+} //equivalente a init, self/ 'c' (poderia ser qualquer nome mas em go é a primeira letra do ponteiro) é o endereço de *ContaCorrente // ENtrada   //retorno
+func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
+	podeSacar := valorDoSaque > 0 //&& valorDoSaque <= c.saldo
 	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso! "
+		c.saldo += valorDoSaque
+		return "Deposito realizado com sucesso! ", c.saldo
 	} else {
-		return "Saldo insuficiente! "
+		return "Saldo insuficiente! ", c.saldo
+	}
+}
+func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64){
+	
+	if valorDeposito > 0 {
+		c.saldo += valorDeposito
+		return "Saque realizado com sucesso! seu novo saldo é ", c.saldo
+	} else {
+		return "Saldo insuficiente! seu saldo é ", c.saldo
 	}
 }

25817ff2db82c0ea5cd6a9bf22be60d66f5e5aaf
AndrewDevos <andrew.cottadevos@gmail.com>
2023-11-13 22:46:47 -0300

    poupança adicionada

--- contas/contaPoupanca.go
+++ contas/contaPoupanca.go
@@ -1,27 +1,27 @@
 package contas
 
-import "banco/clientes"
+import (
+	"banco/clientes"
+)
 
-// No terminal, entre na pasta raíz do seu projeto () e digite o comando [ go mod init "NOMEdoPROJETO"]
-
-type ContaCorrente struct {
+type ContaPoupanca struct {
 	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
+	Operacao      int
 	saldo         float64
 }
 
-// equivalente a init, self/ 'c' (poderia ser qualquer nome mas em go é a primeira letra do ponteiro) é o endereço de *ContaCorrente // ENtrada   //retorno
-func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
+func (c *ContaPoupanca) Sacar(valorDoSaque float64) (string, float64) {
 	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 	if podeSacar {
-		c.saldo += valorDoSaque
+		c.saldo -= valorDoSaque
 		return "Deposito realizado com sucesso! ", c.saldo
 	} else {
 		return "saldo insuficiente! ", c.saldo
 	}
 }
-func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64) {
+func (c *ContaPoupanca) Depositar(valorDeposito float64) (string, float64) {
 
 	if valorDeposito > 0 {
 		c.saldo += valorDeposito
@@ -30,7 +30,7 @@ func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64) {
 		return "saldo insuficiente! seu saldo é ", c.saldo
 	}
 } //nao usar new e usar ponteiro
-func (c *ContaCorrente) Transferir(valorDaTranferencia float64, contaDestino *ContaCorrente) bool {
+func (c *ContaPoupanca) Transferir(valorDaTranferencia float64, contaDestino *ContaPoupanca) bool {
 	if valorDaTranferencia < c.saldo && valorDaTranferencia > 0 {
 		c.saldo -= valorDaTranferencia
 		contaDestino.Depositar(valorDaTranferencia)
@@ -41,6 +41,7 @@ func (c *ContaCorrente) Transferir(valorDaTranferencia float64, contaDestino *Co
 	}
 
 }
-func (c *ContaCorrente) ObterSaldo() float64 {
+func (c *ContaPoupanca) ObterSaldo() float64 {
 	return c.saldo
 }
+
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
-
-	"banco/clientes"
 	"banco/contas"
+	"fmt"
 )
 
 func main() {
@@ -20,14 +18,36 @@ func main() {
 	// contaDoGustavo.titular = "Gustavo"
 	// contaDoGustavo.saldo = 100
 
-	contaDaSilvia := contas.ContaCorrente{Titular: clientes.Titular{}, Saldo: 300}
-	contaDoGustavo := contas.ContaCorrente{Titular: clientes.Titular{}, Saldo: 100}
+	// contaDaSilvia := contas.ContaCorrente{Titular: clientes.Titular{}, Saldo: 300}
+	// contaDoGustavo := contas.ContaCorrente{Titular: clientes.Titular{}, Saldo: 100}
 
-	fmt.Println(contaDoGustavo)
+	// fmt.Println(contaDoGustavo)
 	// usar & comercial pro ponteiro
-	status := contaDaSilvia.Transferir(200, &contaDoGustavo)
-	fmt.Println(status)
-	fmt.Println("o novo Saldo do Gustavo é", contaDoGustavo.Saldo)
-	fmt.Println("o novo Saldo da Silvia é", contaDaSilvia.Saldo)
+	// status := contaDaSilvia.Transferir(200, &contaDoGustavo)
+	// fmt.Println(status)
+	// fmt.Println("o novo Saldo do Gustavo é", contaDoGustavo.Saldo)
+	// fmt.Println("o novo Saldo da Silvia é", contaDaSilvia.Saldo)
+
+
+	// clienteBruno := clientes.Titular{"Bruno","823.765.400-00", "Dev"}
+	// contaDoBruno := contas.ContaCorrente{clienteBruno, 250, 320005, 100}
+	// fmt.Println(clienteBruno)
+	// fmt.Println(contaDoBruno)
+
+	// clienteExemplo := clientes.Titular{}
+	// contaExemplo := contas.ContaCorrente{}
+	// contaExemplo.Depositar(100)
+	// fmt.Println(clienteExemplo)
+	// fmt.Println(contaExemplo.ObterSaldo())
+
+	
+
+	contaDoDenis := contas.ContaPoupanca{}
+	fmt.Println(contaDoDenis)
+	contaDoDenis.Depositar(100)
+	fmt.Println(contaDoDenis.ObterSaldo())
+	
+	contaDoDenis.Sacar(54)
+	fmt.Println(contaDoDenis.ObterSaldo())
 
 }
